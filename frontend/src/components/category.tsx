e9519b32eced55a9fe22779d83e71448c3140bb1
anasriad <riadanas2000@gmail.com>
2024-01-27 21:44:11 +0000

    ready for deployment

--- frontend/src/components/category.tsx
+++ frontend/src/components/category.tsx
@@ -1,57 +1,58 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { motion } from 'framer-motion';
-import 'swiper/css/bundle';
+import football from '../assets/football.png'
+import basketball from '../assets/basketball.png'
 
-export default function Category() {
-  return (
-    <>
-      <motion.div
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0.5 }}
-        transition={{ delay: 0.5 }}
-        style={{
-            padding: '50px',
-            position:'absolute',
-          transform: 'translate(-50%, -50%)',
-          zIndex: 1000,
-        }}
-      >
-        <div
-          className="bg-white p-6 rounded-md shadow-md"
-          style={{
-            backdropFilter: 'blur(5px)',
-          }}
-        >
+import 'swiper/css'
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
-          {/* Swiper component */}
-          <div className="text-center">
+interface Props{
+  onClose: ()=>void
+}
+
+export default function Category(props: Props) {
+  const {onClose} = props
+  return <>
+
+    <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-screen bg-slate-300 bg-opacity-40">
+      <div className="relative p-4 w-full max-w-2xl max-h-full">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="flex items-center justify-center p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+              Choose Your Favourite Sport al'batal(a)
+            </h3>
+          </div>
+          <div className=" p-48 md:p-10 space-y-4">
             <Swiper
               modules={[Navigation, Pagination, Scrollbar, A11y]}
               spaceBetween={50}
               slidesPerView={1}
               navigation
-              scrollbar={{ draggable: true }}
+              pagination={{ clickable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log('slide change')}
             >
-              <SwiperSlide>Football</SwiperSlide>
-              <SwiperSlide>BasketBall</SwiperSlide>
-              <SwiperSlide>Tennis</SwiperSlide>
+              <SwiperSlide>
+                <div className=' p-5 flex justify-center'>
+                  <button className=" w-52 hover:scale-125 hover:duration-300"><img src={football} /></button>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className=' p-5 flex justify-center'>
+                  <button className=" w-52 hover:scale-125 hover:duration-300"><img src={basketball} /></button>
+                </div>
+              </SwiperSlide>
             </Swiper>
           </div>
+          <div className=' p-10 flex justify-center'>
+            <button className=' bg-red-600 text-white rounded-xl p-4' onClick={()=>onClose()}>Close</button>
+          </div>
         </div>
-      </motion.div>
-      <div
-        style={{
-          position: 'fixed',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.7)',
-          zIndex: 999, // Lower z-index than the modal
-        }}
-      />
-    </>
-  );
+      </div>
+    </div>
+
+  </>
+
 }
--- frontend/src/pages/ServicePage.tsx
+++ frontend/src/pages/ServicePage.tsx
@@ -1,12 +1,12 @@
 import { BsChevronDoubleDown } from 'react-icons/bs';
 import Clothes from '../assets/clothes.png'
-import lufy from '../assets/lufy.png'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useNavigate } from 'react-router-dom';
 import TypewriterComponent from 'typewriter-effect';
 import Header from '../components/Header';
 import { useState } from 'react';
+import logo from '../assets/Logo2.png'
 import Category from '../components/category';
 export default function Services() {
     const Navigate = useNavigate()
@@ -14,7 +14,7 @@ export default function Services() {
     return <>
         <div className='flex flex-col h-screen gap-16'>
             <Header />
-            {Modal && <Category />}
+            {Modal && <Category onClose={()=>setModal(false)}/>}
             <div className='flex flex-col gap-y-16'>
                 <div className='flex justify-center w-screen font-extrabold text-2xl text-slate-500 animate-pulse'>
                     <TypewriterComponent onInit={(typewriter) => {
@@ -31,41 +31,43 @@ export default function Services() {
                             <img src={Clothes} className='max-w-sm max-h-max'></img>
                             <h1 className='text-green-400 animate-bounc e'><BsChevronDoubleDown /></h1>
                             <h1>Fashion</h1>
+                            <h2 className=' text-red-600'>Soon to be Released, Get a Hint</h2>
                         </div>
                         <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500" onClick={() => setModal(true)}>
                             <img src={Clothes} className='max-w-sm max-h-max'></img>
                             <h1 className='text-green-400 animate-bounc e'><BsChevronDoubleDown /></h1>
                             <h1>Sport</h1>
+                            <h2 className=' text-red-600'>Soon to be Released</h2>
                         </div>
                         <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500">
-                            <img src={lufy} className='max-w-sm max-h-max'></img>
+                            <img src={logo} className='max-w-sm max-h-max'></img>
                             <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
-                            <h1>Carpentry</h1>
+                            <h2 className=' text-red-600'>Soon to be Released</h2>
                         </div>
                         <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500">
-                            <img src={lufy} className='max-w-sm max-h-max'></img>
+                            <img src={logo} className='max-w-sm max-h-max'></img>
                             <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
-                            <h1>Home Decor</h1>
+                            <h2 className=' text-red-600'>Soon to be Released</h2>
                         </div>
                         <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500">
-                            <img src={lufy} className='max-w-sm max-h-max'></img>
+                            <img src={logo} className='max-w-sm max-h-max'></img>
                             <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
-                            <h1>Flyers/Posters</h1>
+                            <h2 className=' text-red-600'>Soon to be Released</h2>
                         </div>
                         <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500">
-                            <img src={lufy} className='max-w-sm max-h-max'></img>
+                            <img src={logo} className='max-w-sm max-h-max'></img>
                             <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
-                            <h1>Images/Photographs</h1>
+                            <h2 className=' text-red-600'>Soon to be Released</h2>
                         </div>
                         <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500">
-                            <img src={lufy} className='max-w-sm max-h-max'></img>
+                            <img src={logo} className='max-w-sm max-h-max'></img>
                             <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
-                            <h1>Accessories</h1>
+                            <h2 className=' text-red-600'>Soon to be Released</h2>
                         </div>
                         <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500" onClick={() => Navigate('/projects')}>
-                            <img src={lufy} className='max-w-sm max-h-max'></img>
+                            <img src={logo} className='max-w-sm max-h-max'></img>
                             <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
-                            <h1>Software Engineering</h1>
+                            <h2 className=' text-red-600'>Soon to be Released, get a Hint</h2>
                         </div>
                     </div>
                 </div>
