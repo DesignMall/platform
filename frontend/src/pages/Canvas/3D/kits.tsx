a77c6d8abf7366919c9c403cf3724b17b1dad270
anasriad <riadanas2000@gmail.com>
2024-02-03 20:55:18 +0000

    n

---
+++ frontend/src/pages/Canvas/3D/kits.tsx
@@ -0,0 +1,22 @@
+import Header from "../../../components/Header"
+export default function Kits(){
+    return <>
+       <div className='flex flex-col h-screen gap-16'>
+          <Header/>
+          <div>
+            <div className=" border rounded-xl p-4 border-green-500 flex flex-col gap-2">
+                <div className=" flex gap-4 items-center">
+                    <input type="text" placeholder="Enter A Name"></input>
+                    <input type="number"></input>
+                </div>
+                <div>
+                   
+                </div>
+            </div>
+            <div>
+                
+            </div>
+          </div>
+       </div>
+    </>
+}
