ce8d282621cd8af66532aa2da46f7394a6fec339
anasriad <riadanas2000@gmail.com>
2024-02-05 18:26:17 +0000

    tryouts

--- frontend/src/pages/ServicePage.tsx
+++ frontend/src/pages/ServicePage.tsx
@@ -11,7 +11,7 @@ export default function Services() {
     const Navigate = useNavigate()
     const [Modal, setModal] = useState(false)
     return <>
-        <div className='flex flex-col h-screen gap-16'>
+        <div className='flex flex-col h-screen gap-16 overflow-x-hidden'>
             <Header />
             {Modal && <Category onClose={() => setModal(false)} />}
             <div className='flex flex-col gap-y-16'>
@@ -24,8 +24,8 @@ export default function Services() {
                             .start()
                     }}></TypewriterComponent>
                 </div>
-                <div className=" ml-16">
-                    <div className="flex flex-row flex-wrap h-screen gap-x-10 gap-y-10">
+                <div className=" flex justify-center">
+                    <div className="flex flex-row flex-wrap h-screen justify-center gap-5">
                         <motion.div
                             initial={{ translateX: -100, rotate: -80, opacity: 0 }}
                             animate={{ translateX: 0, rotate: 0, opacity: 1 }}
@@ -44,7 +44,7 @@ export default function Services() {
                             <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500" onClick={() => setModal(true)}>
                                 <img src={Clothes} className='max-w-sm max-h-max'></img>
                                 <h1 className='text-green-400 animate-bounc e'><BsChevronDoubleDown /></h1>
-                                <h1>Sport</h1>
+                                <h1>Sports</h1>
                                 <h2 className=' text-red-600'>Soon to be Released, Get a Hint</h2>
                             </div>
                         </motion.div>
@@ -66,6 +66,7 @@ export default function Services() {
                             <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500">
                                 <img src={logo} className='max-w-sm max-h-max'></img>
                                 <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
+                                <h1>Architecture</h1>
                                 <h2 className=' text-red-600'>Soon to be Released</h2>
                             </div>
                         </motion.div>
@@ -76,26 +77,7 @@ export default function Services() {
                             <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500">
                                 <img src={logo} className='max-w-sm max-h-max'></img>
                                 <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
-                                <h2 className=' text-red-600'>Soon to be Released</h2>
-                            </div>
-                        </motion.div>
-                        <motion.div
-                            initial={{ translateX: -100, rotate: -80, opacity: 0 }}
-                            animate={{ translateX: 0, rotate: 0, opacity: 1 }}
-                            transition={{ duration: 0.5, delay: 5 * 0.1 }}>
-                            <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500">
-                                <img src={logo} className='max-w-sm max-h-max'></img>
-                                <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
-                                <h2 className=' text-red-600'>Soon to be Released</h2>
-                            </div>
-                        </motion.div>
-                        <motion.div
-                            initial={{ translateX: -100, rotate: -80, opacity: 0 }}
-                            animate={{ translateX: 0, rotate: 0, opacity: 1 }}
-                            transition={{ duration: 0.5, delay: 6 * 0.1 }}>
-                            <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500">
-                                <img src={logo} className='max-w-sm max-h-max'></img>
-                                <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
+                                <h1>Flyers & Posters</h1>
                                 <h2 className=' text-red-600'>Soon to be Released</h2>
                             </div>
                         </motion.div>
@@ -106,6 +88,7 @@ export default function Services() {
                             <div className=" max-w-sm rounded-lg border-green-400 flex flex-col max-h-max border p-8 font-bold items-center hover:cursor-pointer hover:scale-110 hover:bg-green-400 hover:text-white hover:duration-500" onClick={() => Navigate('/projects')}>
                                 <img src={logo} className='max-w-sm max-h-max'></img>
                                 <h1 className='text-green-400 animate-bounce'><BsChevronDoubleDown /></h1>
+                                <h1>Software</h1>
                                 <h2 className=' text-red-600'>Soon to be Released, get a Hint</h2>
                             </div>
                         </motion.div>
